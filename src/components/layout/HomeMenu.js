a115af0f07b5b02aeaa9747835e4c053356a312b
Adelina <adelinaakylbekova565@gmail.com>
2024-05-10 14:24:02 +0600

    add page hero

---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,15 @@
+export default function HomeMenu() {
+    return (
+        <section>
+            <div>
+                <Image></Image>
+            </div>
+            <div className="text-center">
+                <div className="text-center"></div>
+                <h3 className="uppercase text-gray-500 font-semibold leading-4">Check out</h3>
+                <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+            </div>
+        </section>
+
+    );
+}
