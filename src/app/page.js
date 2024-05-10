97f31889b3cc9fb3c0ec86e1fe06c0ef4dfb3fb2
Adelina <adelinaakylbekova565@gmail.com>
2024-05-10 14:23:27 +0600

    add el for pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
+import Header from "@/components/layout/Header";
+import Link from "next/link";
+import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
 
 export default function Home() {
   return (
-   <div>test</div>
+   <>
+   <Header/>
+   <Hero />
+   <HomeMenu/>
+   </>
   );
 }
