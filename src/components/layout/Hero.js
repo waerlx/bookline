1531510798918e0c25ef8cd7b9f1eb9bc3766622
Adelina <adelinaakylbekova565@gmail.com>
2024-05-13 15:17:20 +0600

    add page register

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Right from "../icons/Right"
 export default function Hero() {
     return (
-        <section className="hero">
+        <section className="hero mt-4">
             <div className="py-12">
                 <h1 className="text-4xl font-semibold">Everything <br /> is better <br /> with a&nbsp; <span className="text-primary"> Pizza</span> </h1> 
                 <p className="my-y text-gray-500 text-sm">Pizza is the missing piece that makes every  day complete, a simple yet  delicious joy in life </p>
--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,13 +1,34 @@
+import Image from "next/image";
+import MenuItem from "./menu/MenuItems";
+import SectionHeaders from "./SectionHeaders";
 export default function HomeMenu() {
     return (
         <section>
-            <div>
-                <Image></Image>
+            <div className= "absolute left-0 right-0 justify-start ">
+                <div className=" absolute left-0 -top-[70px] text-left -z-10 ">
+                    <Image src={'/sallad1.png'} width={109} height={189} alt={'sallad'}></Image>
+                </div>
+                <div className="h-48  absolute -top-[100px] right-0 -z-10">
+                    <Image src={'/sallad2.png'} width={107} height={195} alt={'sallad'}></Image>
+                </div>
             </div>
-            <div className="text-center">
-                <div className="text-center"></div>
-                <h3 className="uppercase text-gray-500 font-semibold leading-4">Check out</h3>
-                <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+
+
+            <div className="text-center mb-4">
+                {/* <div className="text-center"></div> */}
+                <SectionHeaders subHeader={'check out'}
+                mainHeader={'Menu'}/>
+            </div>
+
+            <div className="grid grid-cols-3 gap-4">
+               <MenuItem/>
+               <MenuItem/>
+               <MenuItem/>
+               <MenuItem/>
+               <MenuItem/>
+               <MenuItem/>
+               
+
             </div>
         </section>
 
