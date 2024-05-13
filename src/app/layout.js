0465d33be78e919c859ecfd7b9ff72e4d8e62245
Adelina <adelinaakylbekova565@gmail.com>
2024-05-13 15:15:52 +0600

    add components for laoyt

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/Header";
 
-const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700' ] });
+const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700'] });
 
 export const metadata = {
   title: "Create Next App",
@@ -13,9 +14,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
+          <Header />
           {children}
+          <footer className="border-t p-8 text-center text-gray-500 mt-16">
+            &copy; 2023 All rights reserved
+          </footer>
+
+
         </main>
-        </body>
+      </body>
     </html>
   );
 }
