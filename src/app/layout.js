58198e0f2dc6873b4930e35b9bb7f1496510d920
Adelina <adelinaakylbekova565@gmail.com>
2024-05-15 13:52:02 +0600

    change width for main

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-4xl mx-auto p-4">
+        <main className="main  mx-auto p-4">
           <Header />
           {children}
           <footer className="border-t p-8 text-center text-gray-500 mt-16">
