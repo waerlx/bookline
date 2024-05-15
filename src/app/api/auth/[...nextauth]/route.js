2447ff3db870936311d08d2bdce2b17024eb3aed
Adelina <adelinaakylbekova565@gmail.com>
2024-05-15 15:12:18 +0600

    fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import { connectMongoDB } from "@/lib/mongodb";
-import User from "../../../models/User";
+import User from "../../../../models/User";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
