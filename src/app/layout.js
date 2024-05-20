f5e2df0cec02747e108e498c6715e962485bf2a0
Adelina <adelinaakylbekova565@gmail.com>
2024-05-20 15:21:41 +0600

    add fix with authprovider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import {AuthProvider} from "@/app/Providers";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700'] });
 
@@ -14,11 +15,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.className}>
         <main className="main  mx-auto p-4">
+        <AuthProvider>
           <Header />
           {children}
           <footer className="border-t p-8 text-center text-gray-500 mt-16">
             &copy; 2023 All rights reserved
           </footer>
+          </AuthProvider>
 
 
         </main>
