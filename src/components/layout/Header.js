bc935bdce01e13a752cb5a632ae0d083f9b44b94
Adelina <adelinaakylbekova565@gmail.com>
2024-05-20 15:24:54 +0600

    change header and add link for profile

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,29 +1,45 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import Search from "./Search";
 import React from 'react';
 import dynamic from 'next/dynamic';
+import { Button } from "antd";
+import { signOut, useSession } from "next-auth/react"; // Импортируем необходимые модули
+
 const SearchComponent = dynamic(() => import('@/components/layout/Search'), { ssr: false });
+
 export default function Header() {
+  const { data: session, status } = useSession(); // Используем useSession для получения текущей сессии4
+  const user = session?.user;
+  let userName = user?.name || user?.email;
+  if (userName && userName.includes(' ')) {
+    userName = userName.split(' ')[0];
+  }
+
 
   return (
-    <header className="flex items-center justify-between" >
+    <header className="flex items-center justify-between">
       <nav className="flex gap-8 text-gray-500 font-semibold">
         <Link className="text-primary font-semibold text-2xl" href="/">BookLine</Link>
-
         <Link href="/">Home</Link>
         <Link href="">Menu</Link>
         <Link href="/about">About</Link>
         <Link href="">Contact</Link>
-
-
       </nav>
       <SearchComponent />
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-        {/* <Link href={"/register"}>Login</Link> */}
-        <Link href="/login_page" className="bg-primary rounded-full text-white px-8 py-2">Login</Link>
-
+        {status === 'authenticated' ? (
+          <>
+          <Link href="/profile" className="text-primary font-semibold">
+              {userName}
+            </Link>
+          <Button onClick={() => signOut()} className=" bg-primary rounded-full text-white px-8 py-2">Logout</Button>
+          </>
+          
+        ) : (
+          <Link href="/login_page" className="bg-primary rounded-full text-white px-8 py-2">Login</Link>
+        )}
       </nav>
     </header>
   );
