a66dfb60c197cc7fbbec0de34d2dc9a401938e8a
Adelina <adelinaakylbekova565@gmail.com>
2024-05-20 15:23:34 +0600

    connect profile with data

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,22 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import User from "@/models/User";
+import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server"; // Не забудьте импортировать NextResponse
+
+export async function PUT(req) {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    const data = await req.json();
+    const session = await getServerSession(authOptions);
+    const email = session.user.email;
+
+    if ('name' in data) {
+      await User.updateOne({ email }, { name: data.name });
+    }
+    return NextResponse.json({ message: "User updated." }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Error updating user." }, { status: 500 });
+  }
+}
