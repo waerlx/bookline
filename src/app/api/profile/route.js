a22591c35884721448bbeb8575a3a6dc048be7b4
Adelina <adelinaakylbekova565@gmail.com>
2024-05-24 02:57:29 +0600

    correct my error with api profile

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -2,30 +2,27 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import User from "@/models/User";
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server"; // Не забудьте импортировать NextResponse
+import { NextResponse } from "next/server";
+import UserInfo from "@/models/UserInfo";
 
 export async function PUT(req) {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    const data = await req.json();
-    const session = await getServerSession(authOptions);
-    const email = session.user.email;
+  mongoose.connect(process.env.MONGODB_URI);
+  const data = await req.json();
+  const session = await getServerSession(authOptions);
+  const email = session.user.email;
 
-    if ('name' in data) {
-      await User.updateOne({ email }, { name: data.name });
-    }
-    return NextResponse.json({ message: "User updated." }, { status: 201 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ message: "Error updating user." }, { status: 500 });
-  }
+    await User.updateOne({ email }, { name: data.name });
+
+  return Response.json(true);
+
 }
-
 export async function GET() {
   mongoose.connect(process.env.MONGODB_URI);
   const session = await getServerSession(authOptions);
   const email = session.user.email;
   return Response.json(
-    await User.findOne({ email })
-  )
+    await User.findOne({ email },)
+  );
+
+
 }
