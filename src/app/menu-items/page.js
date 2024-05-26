30ead2152b29b073338801ada384999fee0060f8
Adelina <adelinaakylbekova565@gmail.com>
2024-05-27 01:06:47 +0600

    ch style for menu page

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -26,7 +26,7 @@ export default function MenuItemsPage() {
         return 'Not an admin.';
     }
     return (
-        <section className='mt-8 max-w-md mx-auto'>
+        <section className='mt-8 max-w-2xl mx-auto'>
             <UserTabs isAdmin={true} />
             <div className="mt-8">
                 <Link
