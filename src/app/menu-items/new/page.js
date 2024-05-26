92c8656106e27efc5de072fa47b8e7d705fe1e77
Adelina <adelinaakylbekova565@gmail.com>
2024-05-27 01:07:39 +0600

    ch style for new menu item pg

--- src/app/menu-items/new/page.js
+++ src/app/menu-items/new/page.js
@@ -58,7 +58,7 @@ export default function NewMenuItemPage() {
     return (
         <section>
             <UserTabs isAdmin={true} />
-            <div className="max-w-md mx-auto mt-8">
+            <div className="max-w-2xl mx-auto mt-8">
                 <Link
                     className='button flex justify-center gap-2'
                     href={'/menu-items'}>
