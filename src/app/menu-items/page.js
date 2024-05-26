c7757753ad5a5c63c344df250532dc4b9862e303
Adelina <adelinaakylbekova565@gmail.com>
2024-05-26 23:30:41 +0600

    pase page categories

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -1,43 +1,24 @@
 "use client";
-import { useProfile } from '@/components/layout/UseProfile';
+import { useProfile } from '@/components/UseProfile';
+// import Right from "@/components/icons/Right";
 import UserTabs from '@/components/layout/UserTabs';
-import EditableImage from '@/components/layout/EditableImage'
-import { useState } from 'react';
-import { resolve } from 'path';
-import { rejects } from 'assert';
-import toast from 'react-hot-toast';
-export default function MenuItemsPage() {
-    const [image, setImage] = useState('');
-    const { loding, data } = useProfile();
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
+import Link from 'next/link';
+import Right from '@/components/icons/Right';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
-    async function handleFormSubmit(ev) {
-        ev.preventDefault();
-        const data = {name,description,price,image};
-        const savingPromise = new Promise(async (resolve, reject) => {
-            const response = await fetch('/api/menu-items', {
-                method: 'POST',
-                
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json'
-                },
+export default function MenuItemsPage() {
+    const [menuItems, setMenuItems] = useState([]);
+    const { loading, data } = useProfile();
+    useEffect(() => {
+        fetch('/api/menu-items').then(res =>{
+            res.json().then(menuItems    => {
+                setMenuItems(menuItems);
             });
-            if (response.ok) {
-                resolve();
-            } else
-            reject();
-        });
-        await toast.promise(savingPromise, {
-            loading: 'Saving...',
-            success: 'Profile saved!',
-            error: 'Error',
-        });
-        
-    }
-    if (loding) {
+        })
+    }, []);
+
+    if (loading) {
         return <p>Loading user info...</p>;
     }
 
@@ -45,37 +26,35 @@ export default function MenuItemsPage() {
         return 'Not an admin.';
     }
     return (
-        <section>
+        <section className='mt-8 max-w-md mx-auto'>
             <UserTabs isAdmin={true} />
-            <form onSubmit={handleFormSubmit} className='mt-8 max-w-md mx-auto'>
-                <div className="grid gap-4 items-start "
-                    style={{ gridTemplateColumns: '.3fr .7fr' }}>
-                    <div className="">
-                        <EditableImage link={image} setLink={setImage} />
-                    </div>
-                    <div className='grow'>
-                        <label >Item name</label>
-                        <input
-                            value={name}
-                            onChange={ev => setName(ev.target.value)}
-                            type="text" />
+            <div className="mt-8">
+                <Link
+                className='button flex justify-center gap-2'
+                    href={'/menu-items/new'}>
+                    <span>Create new menu item</span>
+                    <Right  />
+                </Link>
 
-                        <label >Description</label>
-                        <input
-                            value={description}
-                            onChange={ev => setDescription(ev.target.value)}
-                            type="text" />
+            </div>
+            <div className="">
+                <h2 className='text-base text-gray-500 mt-8'>Edit menu item:</h2>
+                <div className="grid grid-cols-3 gap-2">
+                {menuItems?.length > 0 && menuItems.map(item => (
+                    <Link href={'/menu-items/edit/'+item._id}  className='bg-gray-200 rounded-lg p-4 '>
+                        <div className="relative ">
+                        <Image className='rounded-md' src={item.image} alt={''} width={200} height={200}></Image>
+                        </div>
+                        <div className="text-center">
+                        {item.name}
 
-                        <label >Price</label>
-                        <input
-                            value={price}
-                            onChange={ev => setPrice(ev.target.value)}
-                            type="text" />
-                        <button className='bg-primary text-white ' type="submit">Save</button>
-                    </div>
-
+                        </div>
+                        
+                    </Link>
+                ))}
                 </div>
-            </form>
+                
+            </div>
         </section>
     )
 }
