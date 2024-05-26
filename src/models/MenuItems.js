ac6290897dc30ced484f77610a06c52286ed5e1e
Adelina <adelinaakylbekova565@gmail.com>
2024-05-26 23:35:43 +0600

    add schema for menu item

--- src/models/MenuItems.js
+++ src/models/MenuItems.js
@@ -1,10 +1,19 @@
 import mongoose, {model, models, Schema} from "mongoose";
+const ExtraPriceSchema = new Schema({
+  name: String,
+  price: Number,
+});
 
 const MenuItemSchema = new Schema({
   image: {type: String},
   name: {type: String},
+  author: {type: String},
+  genre: {type: String},
   description: {type: String},
   price: {type: Number},
+  covers: {type:[ExtraPriceSchema]},
+  paper: {type:[ExtraPriceSchema]},
+
 }, {timestamps: true});
 
 export const MenuItem = models?.MenuItem || model('MenuItem', MenuItemSchema);
