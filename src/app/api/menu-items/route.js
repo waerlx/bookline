c3117acfd10aee7b97b744de2201934b42076d6d
Adelina <adelinaakylbekova565@gmail.com>
2024-05-27 01:05:57 +0600

    add func delete for menu items

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -1,22 +1,29 @@
-import {MenuItem} from "../../../models/MenuItems";
+import { MenuItem } from "../../../models/MenuItems";
 import mongoose from "mongoose";
-export async function POST(req){
+export async function POST(req) {
     mongoose.connect(process.env.MONGODB_URI);
     const data = await req.json();
     const MenuItemDoc = await MenuItem.create(data);
     return Response.json(MenuItemDoc);
 }
-export async function PUT(req){
+export async function PUT(req) {
     mongoose.connect(process.env.MONGODB_URI);
-    const {_id, ...data} = await req.json();
+    const { _id, ...data } = await req.json();
     await MenuItem.findByIdAndUpdate(_id, data);
     return Response.json(true);
 }
 
-export async function GET(req){
+export async function GET(req) {
     mongoose.connect(process.env.MONGODB_URI);
     const MenuItemDoc = await MenuItem.find();
     return Response.json(
         await MenuItem.find()
     );
+}
+export async function DELETE(req) {
+    mongoose.connect(process.env.MONGODB_URI);
+    const url = new URL(req.url);
+    const _id = url.searchParams.get('_id');
+    await MenuItem.deleteOne({ _id });
+    return Response.json(true);
 }
