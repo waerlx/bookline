6dc0a3ee776daf011fe06572fbceeea586459f00
Adelina <adelinaakylbekova565@gmail.com>
2024-05-26 23:29:43 +0600

    add function put get for menu item

--- src/app/api/menu-items/route.js
+++ src/app/api/menu-items/route.js
@@ -6,3 +6,17 @@ export async function POST(req){
     const MenuItemDoc = await MenuItem.create(data);
     return Response.json(MenuItemDoc);
 }
+export async function PUT(req){
+    mongoose.connect(process.env.MONGODB_URI);
+    const {_id, ...data} = await req.json();
+    await MenuItem.findByIdAndUpdate(_id, data);
+    return Response.json(true);
+}
+
+export async function GET(req){
+    mongoose.connect(process.env.MONGODB_URI);
+    const MenuItemDoc = await MenuItem.find();
+    return Response.json(
+        await MenuItem.find()
+    );
+}
